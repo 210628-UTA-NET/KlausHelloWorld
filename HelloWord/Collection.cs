275b8280fa8e3142cd969ed1dadcaf94f6766256
klauslawson <lateklaus.lawsondjecky@revature.net>
2021-06-29 13:55:17 -0500

    Added new files

---
+++ HelloWord/Collection.cs
@@ -0,0 +1,27 @@
+using System;
+using System.Collections.Generic;
+
+namespace HelloWord
+{
+    public class Collections
+    {
+        static List<string> strings = new list<string>();
+        static ArrayList<> arrays = new ArrayList<>();
+
+        public static void CollectionMain()
+        {
+            Console.WriteLine("-- Collection Demo --");
+            strings.Add("first element");
+            strings.Add("second element");
+            strings.Add("third element");
+
+            //A way to go throungh a list
+            foreach(string item in strings)
+            {
+                Console.WriteLine(item);
+            }
+
+            Console.WriteLine("-- ArrayList Demo --");
+        }
+    }
+}
